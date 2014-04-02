197bea16102bcd1873db35eb8cd3ae382cadf1bd
Bogdan <Bogdan@xps>
2014-04-02 01:30:42 -0400

    Added undo functionality

---
+++ infodb.php
@@ -0,0 +1,6 @@
+<?php
+$db_hostname = 'localhost';
+$db_database = 'db_name'; //your database name will depend on your cPanel login on hostgator
+$db_username = 'user_login'; //your database user name will depend on your cPanel login on hostgator
+$db_password = 'password'; //replace with your own password.
+?>
